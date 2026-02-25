26e8d9b665c0b01cf2744df70eb36001b16a2545
root <root@sevenolives-empire.domain.tld>
2026-02-25 13:24:59 +0000

    Remove placeholder expiring domains section from homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,4 @@
 import { SearchDomains } from './components/SearchDomains';
-import { ExpiringDomains } from './components/ExpiringDomains';
 import { Features } from './components/Features';
 import { Stats } from './components/Stats';
 import { HowItWorks } from './components/HowItWorks';
@@ -80,10 +79,6 @@ export default function Home() {
 
       <Stats />
       
-      {/* Expiring Domains Section */}
-      <section style={{ padding: '80px 20px', maxWidth: '900px', margin: '0 auto' }}>
-        <ExpiringDomains limit={6} />
-      </section>
       
       <HowItWorks />
       <Features />
